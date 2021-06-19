94a80453831b0a412b53759b2a4da28193eb4d24
charlesfrye <charles@wandb.com>
2021-06-18 21:48:21 -0700

    fixes name of matrix in q06

--- math-for-ml/01_linearalgebra/utils/tests/q06.py
+++ math-for-ml/01_linearalgebra/utils/tests/q06.py
@@ -11,16 +11,16 @@ test = {
                     >>> isinstance(set_second_to_zero_and_repeat_3_2, np.ndarray)
                     True
                     >>> ## it should have two dimensions
-                    >>> set_second_to_zero_and_repeat_3_2.ndim
+                    >>> set_second_to_zero_and_repeat_3.ndim
                     2
                     >>> ## it takes length-2 vectors as input
-                    >>> set_second_to_zero_and_repeat_3_2.shape[1]
+                    >>> set_second_to_zero_and_repeat_3.shape[1]
                     2
                     >>> ## its output has 3 times the length of the input
-                    >>> set_second_to_zero_and_repeat_3_2.shape[0] // 3
+                    >>> set_second_to_zero_and_repeat_3.shape[0] // 3
                     2
                     >>> ## its output is three copies of the input
-                    >>> set_second_to_zero_and_repeat_3_2 @ [1., 2.]
+                    >>> set_second_to_zero_and_repeat_3 @ [1., 2.]
                     array([1., 0., 1., 0., 1., 0.])
                     """,
                     "hidden": False,
