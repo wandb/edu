90eee77368f1686836121ba897e7ff6e47eb98fd
charlesfrye <charles@wandb.com>
2021-06-22 17:26:41 -0700

    sets up probability autograding, nb linting

---
+++ math-for-ml/03_probability/utils/tests/q01.py
@@ -0,0 +1,27 @@
+test = {
+    "name": "pass",
+    "points": 1,
+    "suites": [
+        {
+            "cases": [
+                {
+                    "code": r"""
+                    >>> # TESTS BEGIN HERE
+                    """,
+                    "hidden": False,
+                    "locked": False
+                },
+                {
+                    "code": r"""
+                    >>> # TESTS BEGIN HERE
+                    """,
+                    "hidden": False,
+                    "locked": False
+                },
+            ],
+            "setup": r"""
+            """,
+            "teardown": r"""
+            """,
+            "type": "doctest"}]
+        }
