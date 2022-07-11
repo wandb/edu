9dfd4bd7f1c5f0c65910ecaed24e7458cbc215cf
kldarek <darek.kleczek@gmail.com>
2022-07-11 13:01:39 +0000

    evaluation

--- model-dev-course/lesson3/eval.py
+++ model-dev-course/lesson3/eval.py
@@ -13,21 +13,16 @@ def prepare_data(PROCESSED_DATA_AT):
     processed_dataset_dir = Path(processed_data_at.download())
     df = pd.read_csv(processed_dataset_dir / 'data_split.csv')
     df = df[df.stage != 'train'].reset_index(drop=True) # for eval we need test and validation datasets only
-    df['valid'] = df.stage == 'valid'
+    df['test'] = df.stage == 'test'
     return df, processed_dataset_dir
-
-def get_dataloaders(df, path, seed, target_column, img_size, bs):
-    "Get test/valid PyTorch Dataloaders"
-    set_seed(seed, reproducible=True)
-    dls = ImageDataLoaders.from_df(df, path=path, seed=seed, fn_col='file_name', label_col=target_column, 
-                                   valid_col='valid', item_tfms=Resize(img_size), bs=bs)
-    return dls
 
 def eval(cfg):
     
+    set_seed(cfg.seed, reproducible=True)
+    
     run = wandb.init(project=cfg.PROJECT_NAME, entity=cfg.ENTITY, job_type="evaluation", tags=['staging'])
 
-    artifact = run.use_artifact('wandb_course/model-registry/Lemon Mold Detector:v0', type='model')
+    artifact = run.use_artifact('wandb_course/model-registry/Lemon Mold Detector:staging', type='model')
     artifact_dir = artifact.download()
     model_path = Path(artifact_dir).absolute()/'model'
     
@@ -43,9 +38,10 @@ def eval(cfg):
     dls = ImageDataLoaders.from_df(df, path=path,
                                    fn_col='file_name', 
                                    label_col=cfg.target_column, 
-                                   valid_col='valid', 
+                                   valid_col='test', 
                                    item_tfms=Resize(cfg.img_size), 
-                                   bs=cfg.bs
+                                   bs=cfg.bs,
+                                   shuffle=False
                                   )
     learn = vision_learner(dls, 
                            cfg.arch,
@@ -53,19 +49,19 @@ def eval(cfg):
 
     learn.load(model_path)
 
-    _, val_accuracy, val_precision, val_recall, val_f1 = learn.validate(ds_idx=1)
-    _, tst_accuracy, tst_precision, tst_recall, tst_f1 = learn.validate(ds_idx=0)
+    val_loss, val_accuracy, val_precision, val_recall, val_f1 = learn.validate(ds_idx=0)
+    tst_loss, tst_accuracy, tst_precision, tst_recall, tst_f1 = learn.validate(ds_idx=1)
 
-    wandb.log({
-        "val/accuracy": val_accuracy,
-        "val/precision": val_precision,
-        "val/recall": val_recall,
-        "val/f1": val_f1,
-        "tst/accuracy": tst_accuracy,
-        "tst/precision": tst_precision,
-        "tst/recall": tst_recall,
-        "tst/f1": tst_f1,        
-    })
+    wandb.summary["valid_loss"] = val_loss
+    wandb.summary["accuracy"] = val_accuracy
+    wandb.summary["precision_score"] = val_precision
+    wandb.summary["recall_score"] = val_recall
+    wandb.summary["f1_score"] = val_f1
+    wandb.summary["tst/accuracy"] = tst_accuracy
+    wandb.summary["tst/precision_score"] = tst_precision
+    wandb.summary["tst/recall_score"] = tst_recall
+    wandb.summary["tst/f1_score"] = tst_f1
+    wandb.summary["tst/loss"] = tst_loss
 
     run.finish()
   
@@ -76,6 +72,7 @@ if __name__ == '__main__':
     default_cfg.ENTITY = 'wandb_course'
     default_cfg.PROCESSED_DATA_AT = 'lemon_dataset_split_data:latest'
     default_cfg.target_column = 'mold'
+    default_cfg.seed = 42
 
     eval(default_cfg)
 
