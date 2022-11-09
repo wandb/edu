d45fc712b7f711a1cb49ad97df3ec575d274d2e1
kldarek <darek.kleczek@gmail.com>
2022-11-09 23:13:05 +0000

    simpler sample

--- mlops-001/lesson1/params.py
+++ mlops-001/lesson1/params.py
@@ -1,8 +1,5 @@
 WANDB_PROJECT = "BDD100k"
 ENTITY = None # 'av-team'
-BDD_CLASSES = {i:c for i,c in enumerate(['road', 'sidewalk', 'building', 'wall', 'fence', 'pole', 'traffic light',
-    'traffic sign', 'vegetation', 'terrain', 'sky', 'person', 'rider', 'car',
-    'truck', 'bus', 'train', 'motorcycle', 'bicycle', 'void'
-])}
-RAW_DATA_AT = 'bdd_sample_1k'
-PROCESSED_DATA_AT = 'bdd_sample_1k_split'
+BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
+RAW_DATA_AT = 'bdd_simple_1k'
+PROCESSED_DATA_AT = 'bdd_simple_1k_split'
