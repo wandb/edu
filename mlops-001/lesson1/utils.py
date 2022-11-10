2be18df791cf792cc786973c34ebd164d61d8c6b
kldarek <darek.kleczek@gmail.com>
2022-11-10 12:16:13 +0000

    adding IOU metrics

--- mlops-001/lesson1/utils.py
+++ mlops-001/lesson1/utils.py
@@ -2,13 +2,13 @@ import wandb
 
 from fastai.vision.all import *
 
-def faccuracy(inp, targ):
-    "Foreground accuracy per item"
-    return foreground_acc(inp.unsqueeze(0), targ.unsqueeze(0)).item()
+import params
 
-def dice_per_class(inp, targ):
-    "Compute dice per class"
-    dice_scores = []
+CLASS_INDEX = {v:k for k,v in params.BDD_CLASSES.items()}
+
+def iou_per_class(inp, targ):
+    "Compute iou per class"
+    iou_scores = []
     eps = 1e-6
     for c in range(inp.shape[0]):
         dec_preds = inp.argmax(dim=0)
@@ -17,17 +17,16 @@ def dice_per_class(inp, targ):
         c_inter = (p * t).float().sum().item()
         c_union = (p + t).float().sum().item()
         if c_union:
-            dice_scores.append(2.0 * c_inter / (c_union + eps))
+            iou_scores.append(c_inter / (c_union - c_inter + eps))
         else:
-            dice_scores.append(-1)
-    return dice_scores
+            iou_scores.append(-1)
+    return iou_scores
 
 def create_row(sample, pred_label, prediction, class_labels):
     """"A simple function to create a row of (img, target, prediction, and scores...)"""
     (image, label) = sample
     # compute metrics
-    dice_scores = dice_per_class(prediction, label)
-    facc = faccuracy(prediction, label)
+    iou_scores = iou_per_class(prediction, label)
     image = image.permute(1, 2, 0)
     row =[wandb.Image(
                 image,
@@ -42,23 +41,21 @@ def create_row(sample, pred_label, prediction, class_labels):
                     },
                 },
             ),
-            *dice_scores,
-            facc,
+            *iou_scores,
     ]
     return row
 
-def create_dice_table(samples, outputs, predictions, class_labels):
+def create_iou_table(samples, outputs, predictions, class_labels):
     "Creates a wandb table with predictions and targets side by side"
 
     def _to_str(l):
-        return [str(x) for x in l]
+        return [f'{str(x)} IoU' for x in l]
     
     items = list(zip(samples, outputs, predictions))
     
     table = wandb.Table(
         columns=["Image"]
-        + _to_str(class_labels.values())
-        + ["Foreground Acc"],
+        + _to_str(class_labels.values()),
     )
     # we create one row per sample
     for item in progress_bar(items):
@@ -76,3 +73,56 @@ def get_predictions(learner, test_dl=None, max_n=None):
         tuplify(inputs) + tuplify(targets), outputs, show=False, max_n=max_n
     )
     return samples, outputs, predictions
+
+    def value(self): return self.inter/(self.union-self.inter) if self.union > 0 else None
+
+class MIOU(DiceMulti):
+    @property
+    def value(self):
+        binary_iou_scores = np.array([])
+        for c in self.inter:
+            binary_iou_scores = np.append(binary_iou_scores, \
+                                          self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan)
+        return np.nanmean(binary_iou_scores)
+    
+class BackgroundIOU(DiceMulti):
+    @property
+    def value(self):
+        c = CLASS_INDEX['background']
+        return self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan
+
+class RoadIOU(DiceMulti):
+    @property
+    def value(self):
+        c = CLASS_INDEX['road']
+        return self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan
+    
+class TrafficLightIOU(DiceMulti):
+    @property
+    def value(self):
+        c = CLASS_INDEX['traffic light']
+        return self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan
+    
+class TrafficSignIOU(DiceMulti):
+    @property
+    def value(self):
+        c = CLASS_INDEX['traffic sign']
+        return self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan   
+    
+class PersonIOU(DiceMulti):
+    @property
+    def value(self):
+        c = CLASS_INDEX['person']
+        return self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan
+    
+class VehicleIOU(DiceMulti):
+    @property
+    def value(self):
+        c = CLASS_INDEX['vehicle']
+        return self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan
+    
+class BicycleIOU(DiceMulti):
+    @property
+    def value(self):
+        c = CLASS_INDEX['bicycle']
+        return self.inter[c]/(self.union[c]-self.inter[c]) if self.union[c] > 0 else np.nan
