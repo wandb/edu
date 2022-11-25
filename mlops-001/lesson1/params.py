0686760924ee790c2400e6f8691d623374dbc14a
kldarek <darek.kleczek@gmail.com>
2022-11-25 17:06:03 +0000

    params cleanup

--- mlops-001/lesson1/params.py
+++ mlops-001/lesson1/params.py
@@ -1,5 +1,5 @@
 WANDB_PROJECT = "mlops-course-001"
-ENTITY = 'av-team'
+ENTITY = None # set this to team name if working in a team
 BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
 RAW_DATA_AT = 'bdd_simple_1k'
 PROCESSED_DATA_AT = 'bdd_simple_1k_split'
