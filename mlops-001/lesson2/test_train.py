8574c37adfbbe34a2cb945edcf948bfc2b15c2c9
kldarek <darek.kleczek@gmail.com>
2023-01-20 11:58:39 +0000

    tests in lesson2

---
+++ mlops-001/lesson2/test_train.py
@@ -0,0 +1,19 @@
+import train
+from fastai.vision.all import SimpleNamespace
+
+def test_train():
+    default_config = SimpleNamespace(
+        framework="fastai",
+        img_size=60, # small size for the smoke test
+        batch_size=2, # low bs to fit on CPU if needed
+        augment=True, # use data augmentation
+        epochs=1,
+        lr=2e-3,
+        pretrained=True,  # whether to use pretrained encoder,
+        mixed_precision=True, # use automatic mixed precision
+        arch="resnet18",
+        seed=42,
+        log_preds=False,
+    )
+    train.train(default_config)
+    
