1d76359670bef984755c5ecc3757712e7bd43c2e
Nov05 <Nov05@users.noreply.github.com>
2023-03-07 20:35:53 -0600

    2023-03-07 update 'entity' and 'raw-data-at'

--- mlops-001/lesson2/params.py
+++ mlops-001/lesson2/params.py
@@ -1,5 +1,5 @@
 WANDB_PROJECT = "mlops-course-001"
-ENTITY = 'av-team'
+ENTITY = 'novemberfifth' #'av-team'
 BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
-RAW_DATA_AT = 'av-team/mlops-course-001/bdd_simple_1k'
+RAW_DATA_AT = 'novemberfifth/mlops-course-001/bdd_simple_1k' #'av-team/mlops-course-001/bdd_simple_1k'
 PROCESSED_DATA_AT = 'av-team/mlops-course-001/bdd_simple_1k_split'
