e3fa8040cccd7d6c695c0f6a9d2646e619287d96
Nov05 <Nov05@users.noreply.github.com>
2023-03-11 06:21:41 -0600

    2023-03-11 update "PROCESSED_DATA_AT"

--- mlops-001/lesson2/params.py
+++ mlops-001/lesson2/params.py
@@ -2,4 +2,4 @@ WANDB_PROJECT = "mlops-course-001"
 ENTITY = 'novemberfifth' #'av-team'
 BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
 RAW_DATA_AT = 'novemberfifth/mlops-course-001/bdd_simple_1k' #'av-team/mlops-course-001/bdd_simple_1k'
-PROCESSED_DATA_AT = 'av-team/mlops-course-001/bdd_simple_1k_split'
+PROCESSED_DATA_AT =  'novemberfifth/mlops-course-001/bdd_simple_1k_split' # 'av-team/mlops-course-001/bdd_simple_1k_split'
