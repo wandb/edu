971ef92ee35ecaaf6b5a4902d11804540af60879
Thomas Capelle <tcapelle@pm.me>
2023-08-24 10:47:22 +0000

    updated params for personal users

--- pyimagesearch/params.py
+++ pyimagesearch/params.py
@@ -1,6 +1,6 @@
 # W&B parameters
-PROJECT_NAME = "wandb_course"
-ENTITY = "pyimagesearch"  # change this to you wandb username
+PROJECT_NAME = "pis_course"
+ENTITY = None  # change this to you wandb team/entity name
 
 # ENTITY = None
 
@@ -10,4 +10,5 @@ RAW_DATA_FOLDER = "lemon-dataset"
 IMAGES_FOLDER = "images"
 ANNOTATIONS_FILE = "annotations/instances_default.json"
 ARTIFACT_NAME = "lemon_data"
-DATA_AT = f"{ENTITY}/{PROJECT_NAME}/{ARTIFACT_NAME}:v0"
+DATA_AT = f"{PROJECT_NAME}/{ARTIFACT_NAME}:v0"
+# DATA_AT = f"{ENTITY}/{PROJECT_NAME}/{ARTIFACT_NAME}:v0" # if Entity is not None
