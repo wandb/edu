4f20dbcfe6a4a94a0bf60b6f40535904e8410063
mrmer1 <meor.amer@gmail.com>
2024-09-23 14:16:14 +0800

    misc updates

--- rag-advanced/notebooks/rag_tooluse/tool_def_v1.py
+++ rag-advanced/notebooks/rag_tooluse/tool_def_v1.py
@@ -5,47 +5,89 @@ from langchain_experimental.utilities import PythonREPL
 
 
 def search_developer_docs(query):
-    developer_docs = [{"text" : "What is a W&B Run?\nA W&B run is a single unit of computation logged by W&B, representing an atomic element of your project."},
-                      
-    {"text" : "How do I view a specific run?\nTo view a run, navigate to the W&B App UI, select the relevant project, and then choose the run from the 'Runs' table."},
-    
-    {"text" : "What are Artifact Outputs?\nArtifact Outputs refer to any artifacts produced by a run."},
-    
-    {"text" : "How can I do hyperparameter search quickly?\nUse W&B Sweeps to automate hyperparameter search and visualize rich, interactive experiment tracking."}
+    developer_docs = [
+        {
+            "text": "What is a W&B Run?\nA W&B run is a single unit of computation logged by W&B, representing an atomic element of your project."
+        },
+        {
+            "text": "How do I view a specific run?\nTo view a run, navigate to the W&B App UI, select the relevant project, and then choose the run from the 'Runs' table."
+        },
+        {
+            "text": "What are Artifact Outputs?\nArtifact Outputs refer to any artifacts produced by a run."
+        },
+        {
+            "text": "How can I do hyperparameter search quickly?\nUse W&B Sweeps to automate hyperparameter search and visualize rich, interactive experiment tracking."
+        },
     ]
 
     return {"developer_docs": developer_docs}
-  
+
+
 def search_internet(query: str) -> dict:
     tool = TavilySearchResults(
         max_results=5,
         search_depth="advanced",
         include_answer=True,
-        include_raw_content=True
+        include_raw_content=True,
     )
     documents = tool.invoke({"query": query})
-    
+
     return {"documents": documents}
 
-def search_code_examples(query: str, file_type: str = None, programming_language: str = None, language: str = None) -> dict:
-    
+
+def search_code_examples(
+    query: str,
+    file_type: str = None,
+    programming_language: str = None,
+    language: str = None,
+) -> dict:
+
     code_examples = [
-        {"content": "Selecting Hyperparameters with Sweeps (Keras)", "file_type": "py", "language": "en"},
-        {"content": "Interactive W&B Charts Inside Jupyter", "file_type": "ipynb", "language": "en"},
+        {
+            "content": "Selecting Hyperparameters with Sweeps (Keras)",
+            "file_type": "py",
+            "language": "en",
+        },
+        {
+            "content": "Interactive W&B Charts Inside Jupyter",
+            "file_type": "ipynb",
+            "language": "en",
+        },
         {"content": "ロギングメディア", "file_type": "py", "language": "ja"},
-        {"content": "テーブルを使用してデータセットと予測を視覚化する", "file_type": "ipynb", "language": "ja"},
-        {"content": "Model/Data Versioning with Artifacts (PyTorch)", "file_type": "ipynb", "language": "en"},
-        {"content": "Create a hyperparameter search with W&B PyTorch integration", "file_type": "ipynb", "language": "en"},
-        {"content": "Get started with W&B Weave", "file_type": "ipynb", "language": "en"}
+        {
+            "content": "テーブルを使用してデータセットと予測を視覚化する",
+            "file_type": "ipynb",
+            "language": "ja",
+        },
+        {
+            "content": "Model/Data Versioning with Artifacts (PyTorch)",
+            "file_type": "ipynb",
+            "language": "en",
+        },
+        {
+            "content": "Create a hyperparameter search with W&B PyTorch integration",
+            "file_type": "ipynb",
+            "language": "en",
+        },
+        {
+            "content": "Get started with W&B Weave",
+            "file_type": "ipynb",
+            "language": "en",
+        },
     ]
-    
+
     filtered_code_examples = [
-        item for item in code_examples
-        if (item['file_type'] == file_type if file_type else True) and
-        (item['programming_language'] == programming_language if programming_language else True) and
-        (item['language'] == language if language else True)
+        item
+        for item in code_examples
+        if (item["file_type"] == file_type if file_type else True)
+        and (
+            item["programming_language"] == programming_language
+            if programming_language
+            else True
+        )
+        and (item["language"] == language if language else True)
     ]
-    
+
     return {"code_examples": filtered_code_examples}
 
 
@@ -60,8 +102,8 @@ python_tool.name = "python_interpreter"
 
 class ToolInput(BaseModel):
     code: str = Field(description="Python code to execute.")
-    
-    
+
+
 python_tool.args_schema = ToolInput
 
 
@@ -70,54 +112,54 @@ def analyze_evaluation_results(code: str) -> dict:
     Function to run given python code
     """
     input_code = ToolInput(code=code)
-    return {'python_answer': python_tool.func(input_code.code)}
-    
+    return {"python_answer": python_tool.func(input_code.code)}
+
+
 ### ------------------------------------------------------------------------------------------
 search_tools = [
     {
-      "name": "search_developer_docs",
-      "description": "Searches the Weights & Biases developer documentation. Use this tool for queries related to the Weights & Biases API, SDKs, or other developer resources.",
-      "parameter_definitions": {
-        "query": {
-          "description": "The search query.",
-          "type": "str",
-          "required": True
-        }
-      }
+        "name": "search_developer_docs",
+        "description": "Searches the Weights & Biases developer documentation. Use this tool for queries related to the Weights & Biases API, SDKs, or other developer resources.",
+        "parameter_definitions": {
+            "query": {
+                "description": "The search query.",
+                "type": "str",
+                "required": True,
+            }
+        },
     },
     {
-      "name": "search_internet",
-      "description": "Returns a list of relevant document snippets for a textual query retrieved from the internet. Use this tool for general queries about Weights & Biases or others.",
-      "parameter_definitions": {
-        "query": {
-          "description": "Query to search the internet with. If you have more than one query, call this tool multiple times.",
-          "type": "str",
-          "required": True
-        }
-      }
+        "name": "search_internet",
+        "description": "Returns a list of relevant document snippets for a textual query retrieved from the internet. Use this tool for general queries about Weights & Biases or others.",
+        "parameter_definitions": {
+            "query": {
+                "description": "Query to search the internet with. If you have more than one query, call this tool multiple times.",
+                "type": "str",
+                "required": True,
+            }
+        },
     },
     {
-      "name": "search_code_examples",
-      "description": "Searches code examples and tutorials on using Weights & Biases.",
-      "parameter_definitions": {
-        "file_type": {
-          "description": "The file format of the code examples to search for. Possible enum values: py, ipynb, null.",
-          "type": "str",
-          "required": False
+        "name": "search_code_examples",
+        "description": "Searches code examples and tutorials on using Weights & Biases.",
+        "parameter_definitions": {
+            "file_type": {
+                "description": "The file format of the code examples to search for. Possible enum values: py, ipynb, null.",
+                "type": "str",
+                "required": False,
+            },
+            "language": {
+                "description": "The language to choose based on either the language the user is using (either English or Japanese) or if the user is asking examples of a specific language. Possible enum values: en, ja, null.",
+                "type": "str",
+                "required": False,
+            },
+            "query": {
+                "description": "The query to search for code examples.",
+                "type": "str",
+                "required": True,
+            },
         },
-        "language": {
-          "description": "The language to choose based on either the language the user is using (either English or Japanese) or if the user is asking examples of a specific language. Possible enum values: en, ja, null.",
-          "type": "str",
-          "required": False
-        },
-        "query": {
-          "description": "The query to search for code examples.",
-          "type": "str",
-          "required": True
-        }
-      }
-    }
-
+    },
 ]
 
 analysis_tool = [
@@ -128,8 +170,8 @@ analysis_tool = [
             "code": {
                 "description": "Executable Python code",
                 "type": "str",
-                "required": True
+                "required": True,
             }
-        }
+        },
     }
 ]
